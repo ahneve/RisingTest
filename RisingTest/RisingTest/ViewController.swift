6da051faaebec61b993dc8bbbbd77d7f25413db3
안소정 <80646857+ahneve@users.noreply.github.com>
2022-09-18 14:35:17 +0900

    [Feat] 프로젝트생성

---
+++ RisingTest/RisingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingTest
+//
+//  Created by 소정의 Mac on 2022/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
